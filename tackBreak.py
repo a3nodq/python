e9f71c5d9d352166f9ddcfb09b2555d85e831ff3
a3nodq <a3nodq@gmail.com>
2018-08-14 23:17:26 +0300

    initial

---
+++ tackBreak.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+#x= total numbers of break
+x = 4
+#y = break count
+y = 0
+
+while (x < y):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=0-7IHOXkiV8")
+    y = x + 1
